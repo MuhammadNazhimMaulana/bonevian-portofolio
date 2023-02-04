32747c8ad97638f54402cd1d17fb762a9ead550e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-02-04 12:27:04 +0700

    Hotfixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return ( 
         <div className="fixed top-0 left-0 w-full">
             <div className="flex bg-white p-4 justify-between">
-                <div className="px-6 text-primary font-bold flex items-center text-lg">Bonevian</div>
+                <div className="px-6 text-primary font-bold flex items-center text-2xl">Bonevian</div>
                 <ul className="flex items-center">
                     <li className="ml-4 text-primary font-bold hover:bg-slate-50 p-3 rounded-full transition-all duration-200"><NavLink to="/">Home</NavLink></li>
                     <li className="ml-4 text-primary font-bold py-3 hover:bg-slate-50 p-3 rounded-full transition-all duration-200"><NavLink to="/about">About</NavLink></li>
